dd39d293c1ec8d06a38e26ae4beb4fa6505ffeab
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 22:43:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-  programmer_hash[:changing_alan][]
+  programmer_hash[:changing_alan][:known_for] = "GUI"
 end
 
 def adding_to_dennis
