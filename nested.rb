4312e34c9d9191fa2bdac2204f366488bb2d121d
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 22:03:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,7 +71,9 @@ def adding_matz
 # return the entire updated hash
 
 	programmer_hash =
- 		{
+ 		{ :yukihiro_matsumoto => {
+ 		  
+ 		},
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
