2543c0d055e727693e338fbd9d0342e72254a690
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 22:24:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,6 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
-  programmer_hash.merge!(:yukihiro_matsumoto)
   return programmer_hash
 end
 
